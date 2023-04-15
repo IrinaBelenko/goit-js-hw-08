989eedc08b28943944d800b442072c0e39714485
IrinaBelenko <118209690+IrinaBelenko@users.noreply.github.com>
2023-04-15 22:25:47 +0300

    01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+const galleryMarkup = createGalleryMarkup(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+galleryRef.insertAdjacentHTML('beforeend', galleryMarkup);
 
-console.log(galleryItems);
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+    `;
+    })
+    .join('');
+}
+
+let gallery = new SimpleLightbox('.gallery a');
+
+gallery.options.captionDelay = 250;
+gallery.options.captionsData = 'alt';
