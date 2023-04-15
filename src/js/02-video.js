28b9be5d037ba2928b3014270684f007bfe85cf9
IrinaBelenko <118209690+IrinaBelenko@users.noreply.github.com>
2023-04-16 00:09:33 +0300

    02-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const timeMemorization = ({ seconds }) => {
+  if (window.localStorage) {
+    localStorage.setItem('videoplayer-current-time', seconds);
+  }
+  console.log(seconds);
+};
+
+const throttleFunc = throttle(timeMemorization, 1000);
+
+player.on('timeupdate', throttleFunc);
+
+player
+  .setCurrentTime(localStorage['videoplayer-current-time'])
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
